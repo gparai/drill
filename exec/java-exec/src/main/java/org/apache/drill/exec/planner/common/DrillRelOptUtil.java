1eb518a3c92de99c17f8939cf7fcd6e1c68a6dab
Gautam Parai <gparai@maprtech.com>
2017-01-06 13:40:23 -0800

    Fix NDV overestimate when groupKey relies on one side of the join

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/common/DrillRelOptUtil.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/common/DrillRelOptUtil.java
@@ -28,6 +28,7 @@ import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.Project;
 import org.apache.calcite.rel.core.Filter;
 import org.apache.calcite.rel.core.TableScan;
+import org.apache.calcite.rel.core.Join;
 import org.apache.calcite.rel.logical.LogicalJoin;
 import org.apache.calcite.rel.rules.ProjectRemoveRule;
 import org.apache.calcite.rel.type.RelDataType;
@@ -211,10 +212,7 @@ public abstract class DrillRelOptUtil {
       } else {
         return true;
       }
-    } /*else if (rel instanceof Filter
-          && findLikeOrRangePredicate(((Filter) rel).getCondition())) {
-      return true;
-    } */else {
+    } else {
       for (RelNode child : rel.getInputs()) {
         if (guessRows(child)) { // at least one child is a guess
           return true;
@@ -239,7 +237,7 @@ public abstract class DrillRelOptUtil {
     return false;
   }
 
-  public static boolean analyzeSimpleEquiJoin(LogicalJoin join, int[] joinFieldOrdinals) {
+  public static boolean analyzeSimpleEquiJoin(Join join, int[] joinFieldOrdinals) {
     RexNode joinExp = join.getCondition();
     if(joinExp.getKind() != SqlKind.EQUALS) {
       return false;
